309927948c54b391704dd9baec52521636e8bd8b
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-01-08 12:17:25 +0530

    Done basic styles for the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,114 @@
-
-
 function App() {
-
   return (
-    <>
+    <div className="p-8">
       <form>
-        <div>
-          <label htmlFor="fname">Full Name</label>
-          <input type="text" name="fname" id="fname" placeholder="Enter Full Name" required />
+        <div className="my-2">
+          <label htmlFor="fname" className="mx-4">
+            Full Name
+          </label>
+          <input
+            type="text"
+            name="fname"
+            id="fname"
+            placeholder="Enter Full Name"
+            className="border border-gray-400 rounded p-1"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="Enter Email Address" required />
+        <div className="my-2">
+          <label htmlFor="email" className="mx-4">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter Email Address"
+            className="border border-gray-400 rounded p-1"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" placeholder="Enter Password" required />
+        <div className="my-2">
+          <label htmlFor="password" className="mx-4">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Password"
+            className="border border-gray-400 rounded p-1"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="color">Your Favorite Color</label>
+        <div className="my-2">
+          <label htmlFor="color" className="mx-4">
+            Your Favorite Color
+          </label>
           <input type="color" name="color" id="color" />
         </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <textarea name="address" id="address" placeholder="Where Can We Find You?" required />
+        <div className="my-2">
+          <label htmlFor="address" className="mx-4">
+            Address
+          </label>
+          <textarea
+            name="address"
+            id="address"
+            placeholder="Where Can We Find You?"
+            className="border border-gray-400 rounded p-1"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="city">City</label>
-          <input type="text" name="city" id="city" placeholder="Enter Your City" required />
+        <div className="my-2">
+          <label htmlFor="city" className="mx-4">
+            City
+          </label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="Enter Your City"
+            className="border border-gray-400 rounded p-1"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="state">State</label>
-          <select name="state" id="state" required >
+        <div className="my-2">
+          <label htmlFor="state" className="mx-4">
+            State
+          </label>
+          <select name="state" id="state" className="border border-gray-400 rounded p-1" required>
             <option value="">Choose...</option>
             <option value="Solid">Solid</option>
             <option value="Liquid">Liquid</option>
             <option value="Gas">Gas</option>
-          </select>  
+          </select>
         </div>
-        <div>
-          <label htmlFor="zip">Pin/Zip</label>
-          <input type="number" name="zip" id="zip" placeholder="Enter Zip/Pin Code" required />
+        <div className="my-2">
+          <label htmlFor="zip" className="mx-4">
+            Pin/Zip
+          </label>
+          <input
+            type="number"
+            name="zip"
+            id="zip"
+            placeholder="Enter Zip/Pin Code"
+            className="border border-gray-400 rounded p-1"
+            required
+          />
         </div>
-        <div>
-          <input type="checkbox" name="agree" id="agree" required />
+        <div className="my-2">
+          <input
+            type="checkbox"
+            name="agree"
+            id="agree"
+            className="mx-4"
+            required
+          />
           <label htmlFor="agree">Say, You Agree!</label>
         </div>
       </form>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
